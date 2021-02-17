ac40303010c15bdaa3bb323afe4ca17d97e887cc
Chandika Srikanth <srikanth@everestlabs.ai>
2021-02-18 01:17:21 +0530

    packer provisioner with ansible local and remove

---
+++ init_ansible_local.sh
@@ -0,0 +1,6 @@
+#cp -rv . ansible/roles/php/files
+mkdir -p ansible/roles/php/files/packer_deploy
+rsync -arv --exclude '*.json' --exclude '.git*' . ansible/roles/php/files/packer_deploy
+echo "ls -lF ansible/roles/php/files/packer_deploy"
+ls -lF ansible/roles/php/files/packer_deploy
+packer build -var-file=vars.json amazon_php_ubuntu_ansible_local_mode.json
--- init_ansible_remote.sh
+++ init_ansible_remote.sh
@@ -3,4 +3,4 @@ mkdir -p ansible/roles/php/files/packer_deploy
 rsync -arv --exclude '*.json' --exclude '.git*' . ansible/roles/php/files/packer_deploy
 echo "ls -lF ansible/roles/php/files/packer_deploy"
 ls -lF ansible/roles/php/files/packer_deploy
-packer build -var-file=vars.json amazon_php_ubuntu.json
+packer build -var-file=vars.json amazon_php_ubuntu_ansible_remote_mode.json
